7b2551a37c320428b4e78dc573800cb8c90e8401
Youssef Aziz <Youssef Aziz@DESKTOP-CJNKAL8>
2025-07-06 03:44:12 +0300

    Clean up DataBase of Docker

--- BackendWebService.Presistence/Data/DbConnection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Persistence.Data
-{
-    public static class DbConnection
-    {
-        public static string DefaultConnection = "Server=.; Database=Sal7ly; Integrated Security = True; TrustServerCertificate=Yes";
-    }
-}
